d72f1f2d58abd12be959a9444187e0fc8c8301d2
brandonposdeley <nahuelposdeley10@gmail.com>
2024-06-03 11:17:10 -0300

    images

--- src/Screens/Contact/Contact.jsx
+++ src/Screens/Contact/Contact.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { AiFillLinkedin } from "react-icons/ai"
 import {CgMail} from "react-icons/cg"
 import { DataContext } from '../../context/SliceContext'
-import imgContact from '../../assets/contact.svg'
 
 const Contact = () => {
   const {textColor,statusColor} = DataContext()
   return (
     <div className='w-11/12 mt-10 m-auto sm:w-3/6 sm:mt-20'>
       <h3 className={`text-center ${textColor} text-2xl font-bold mb-5`}>Contactame</h3>
-      <p className={`font-semibold text-center ${textColor} leading-8 px-10`}>Actualmente estoydisponiblepara sumarme a tú equipo de desarrollo y desenvolver todos mis conocimientos.
+      <p className={`font-semibold text-center ${textColor} leading-8 px-10`}>Actualmente estoy disponible para sumarme a tú equipo de desarrollo y desenvolver todos mis conocimientos.
         Si sos desarrollador/a y tenes alguna traba en algo, no dudes en dejarme un mensaje,
         tal vez te pueda dar una mano 🤜🤛</p>
 
@@ -23,7 +22,6 @@ const Contact = () => {
             <span className={`text-center ${textColor}`}>Linkedin</span>
         </a>
         </div>
-        <img src={imgContact} className='m-auto mt-2 w-7/12 sm:w-3/5' alt='' />
     </div>
   )
 }
--- src/data/data.js
+++ src/data/data.js
@@ -7,26 +7,82 @@ import gitHub from "../assets/github.svg";
 import reactRouter from "../assets/react-router.svg";
 import tailwind from "../assets/tailwindcss.svg";
 import bootstrap from "../assets/bootstrap.svg";
+import nodejs from "../assets/nodejs.png";
+import ts from "../assets/ts.png";
+import eslint from "../assets/eslint.png";
+import recordatorio from "../assets/app-recordatorio.png";
+import loanQuoter from '../assets/loan-quoter.png'
+import reactgift from '../assets/react-gift.png'
+import bancochile from '../assets/banco-chile.png'
+import calories from '../assets/calories.png'
+import rickandmorty from '../assets/rickandmorty.png'
+import pelis from '../assets/pelis.png'
+import marvel from '../assets/marvel.png'
+import calculator from '../assets/calculator.png'
+
 
 export let data = [
   {
-    id: 2,
-    title: "officia porro iure",
-    url: "https://via.placeholder.com/600/24f355",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
-  },
-  {
     id: 3,
     title: "officia porro iure",
-    url: "https://via.placeholder.com/600/24f355",
+    url: loanQuoter,
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
   },
   {
     id: 4,
     title: "officia porro iure",
-    url: "https://via.placeholder.com/600/24f355",
+    url: reactgift,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
+  },
+  {
+    id: 4,
+    title: "officia porro iure",
+    url: bancochile,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
+  },  {
+    id: 4,
+    title: "officia porro iure",
+    url: calories,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
+  },  {
+    id: 4,
+    title: "officia porro iure",
+    url: rickandmorty,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
+  },  {
+    id: 4,
+    title: "officia porro iure",
+    url: pelis,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
+  },  {
+    id: 4,
+    title: "officia porro iure",
+    url: marvel,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
+  },  {
+    id: 4,
+    title: "officia porro iure",
+    url: calculator,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
+  },  {
+    id: 4,
+    title: "officia porro iure",
+    url: pelis,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
+  },
+  {
+    id: 2,
+    title: "officia porro iure",
+    url: recordatorio,
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore provident velit veritatis odio quia eum ullam sequi, suscipit laudantium a.",
   },
@@ -94,6 +150,24 @@ export let myTechnologies = [
     id: 9,
     title: "BOOTSTRAP",
     url: bootstrap,
+    size: "40px",
+  },
+
+  {
+    id: 10,
+    title: "NODE JS",
+    url: nodejs,
+    size: "40px",
+  },
+  {
+    id: 11,
+    title: "TYPESCRIPT",
+    url: ts,
+    size: "40px",
+  },{
+    id: 12,
+    title: "ESLINT",
+    url: eslint,
     size: "40px",
   },
 ];
